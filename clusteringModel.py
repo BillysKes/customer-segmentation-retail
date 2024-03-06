6d50db557161419453851a3e3f104192ae2fae80
BillysKes <73298709+BillysKes@users.noreply.github.com>
2024-03-06 20:03:47 +0200

    Add files via upload

---
+++ clusteringModel.py
@@ -0,0 +1,20 @@
+
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+import pandas as pd
+
+
+df = pd.read_csv('C:\\Users\\vasil\\Downloads\\winsorizedMonetaryFrequencyRFM.csv')
+
+x = df.drop('CustomerID',axis=1)
+x=x.values
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+scaled_df = pd.DataFrame(x_scaled,columns=['Recency','Frequency','Monetary'])
+kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300)
+#exit(scaled_df)
+kmeans.fit(scaled_df)
+labels=kmeans.labels_
+df['cluster'] = labels
+
+#df.to_csv('C:\\Users\\vasil\\Downloads\\winsorizedClusteredv2.csv', index=False)
