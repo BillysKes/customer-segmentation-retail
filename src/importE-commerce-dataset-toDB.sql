3443682a8bf57a532391078835aa6fdc1129cbc5
BillysKes <73298709+BillysKes@users.noreply.github.com>
2024-03-06 22:11:35 +0200

    Update importE-commerce-dataset-toDB.sql

--- src/importE-commerce-dataset-toDB.sql
+++ src/importE-commerce-dataset-toDB.sql
@@ -14,46 +14,14 @@ CREATE TABLE OnlineRetail (
     Country VARCHAR(100)
 );
 ----------------------------------------------------------------
-CREATE TABLE Transactions (
-    InvoiceNo INT,
-    StockCode INT,
-    Description VARCHAR(255),
-    Quantity INT,
-    InvoiceDate DATETIME,
-    UnitPrice DECIMAL(10, 2),
-    CustomerID INT,
-    Country VARCHAR(100),
-    PRIMARY KEY (InvoiceNo)
-);
 
 
-CREATE TABLE Products (
-    StockCode INT PRIMARY KEY,
-    Description VARCHAR(255),
-    UnitPrice DECIMAL(10, 2)
-);
-
-CREATE TABLE Customers (
-    CustomerID INT PRIMARY KEY,
-    Country VARCHAR(100)
-);
-
-CREATE TABLE Transactions_Details (
-    TransactionID INT,
-    StockCode INT,
-    Quantity INT,
-    FOREIGN KEY (TransactionID) REFERENCES Transactions(InvoiceNo),
-    FOREIGN KEY (StockCode) REFERENCES Products(StockCode)
-);
-
-
-CREATE TABLE Cancelled_Transactions (
-    CancelledInvoiceNo INT PRIMARY KEY,
-    OriginalInvoiceNo INT,
-    FOREIGN KEY (OriginalInvoiceNo) REFERENCES Transactions(InvoiceNo)
-);
-
-
+CREATE table CustomersRFM(
+CustomerID VARCHAR(20),
+Recency INT,
+Frequency INT,
+Monetary DECIMAL(10, 2)
+)
 
 
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/data4.csv'
@@ -64,6 +32,8 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (InvoiceNo, StockCode, Description, Quantity, @InvoiceDate, UnitPrice, CustomerID, Country)        
 SET InvoiceDate = STR_TO_DATE(@InvoiceDate, '%m/%d/%Y %H:%i');
+
+
 
 
 
